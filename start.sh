9f65e152308d13a9fa2ecb99b0b92e51168395a3
connquistador <97854426+connquistador@users.noreply.github.com>
2022-12-09 18:06:08 -0600

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while true
+do
+./dynexsolve --algo dynexsolve --opencl-threads auto --opencl-launch auto --url pool.dnx.fairhash.org:2335 --user XwmfP47uuu4XY3YmGcaaLoibEWyLVVgjrGV1agd6j3iGHCjZLoSSBGLTDgnM4X3qjsZrgfgojbnGEZRVepMAQYce28bcV5Txn --pass knqr
+sleep 5
+done
